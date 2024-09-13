88a122bc639bbaaae4200fa5bf470a0bc156b71e
Tobias Haider <tobias.haider@extern.dkbcodefactory.com>
2024-09-13 13:48:56 +0200

    remove github actions

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey: "AIzaSyBbpbr2SEIbE3ekxLMkcd21SlUKKGimKw4",
     authDomain: "smart-work-schedule.firebaseapp.com",
     projectId: "smart-work-schedule",
     storageBucket: "smart-work-schedule.appspot.com",
